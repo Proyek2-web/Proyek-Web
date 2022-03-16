ac2a46b01440b7cb6d9a2907b025532a5d9f4a4d
irfanharfiansyah <irfanharfiansyah37@gmail.com>
2022-03-17 00:06:05 +0700

    add UI total page (Dorong)

--- Proyek2-Web/resources/views/layouts/total.blade.php
+++ Proyek2-Web/resources/views/layouts/total.blade.php
@@ -2,61 +2,68 @@
 @include('layouts.navbar')
 @section('body')
 
-<section class="total">
-    <h1 style="margin-top: 120px">Konfirmasi Pembayaran</h1>
-    <p class="text-center"><i>{{ $detail->customer_name}}</i></p>
+
+<section class="total" style="margin-top: 170px">
+    {{-- <h1 style="margin-top: 120px">Konfirmasi Pembayaran</h1> --}}
 
     <div class="container-sm">
-        <div class="wrap">
-            <div class="row">
-                <div class="col-md-12">
-                    <p class="text-center">Detail Transaksi</p>
-                    <p class="text-center"><i>{{ $detail->reference}}</i></p>
-                    <hr class="mx-auto">
-                </div>
-                <div class="col-md-6">
-                    <p class="text-center">Harga</p>
-                    <p class="text-center">Pengiriman</p>
-                    <p class="text-center">Total Tagihan</p>
-                </div>
-                <div class="col-md-6">
-                    {{-- @foreach ($detail->order_items as $i)
-                    <p class="text-center">Rp.{{ number_format($i->price) }} | {{  $i->quantity}}x</p>
-                    @endforeach --}}
-                    <p class="text-center">Rp. {{ number_format($detail->amount) }}</p>
-                </div> 
-                <div class="col-md-4 mx-auto">
-                    <div class="alert alert-danger d-flex align-items-center" role="alert" style="padding-inline: 30%">
-                        <i class="bi bi-exclamation-circle-fill me-2 "></i>
-                        <div class="">
-                            {{ $detail->status }}
-                        </div>
+        <div class="row">
+            <div class="col-md-7">
+                <div class="wrap justify-content-center">
+                    <div class="first d-flex justify-content-between align-items-center mb-3">
+                        <div class="info"> <span class="d-block name">Thank You, {{ $detail->customer_name}}</span> <span class="order">Order - {{ $detail->reference}}
+                        </span> </div> <img src="https://cdni.iconscout.com/illustration/premium/thumb/connect-banking-card-for-online-payment-3323736-2791565.png" width="400"  class="img-fluid" />
                     </div>
-                </div>
-                
-                <h5 class="mt-5 text-center">Instruksi Pembayaran</h5>
-                @foreach ($detail->instructions as $ts)
-                <div class="accordion" id="accordionExample">
-                    <div class="accordion-item">
-                        <h2 class="accordion-header" id="headingOne">
-                            <button class="accordion-button" type="button" data-bs-toggle="collapse"
-                                data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                                {{ $ts->title }}
-                            </button>
-                        </h2>
-                        <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne"
-                            data-bs-parent="#accordionExample">
-                            <div class="accordion-body">
-                                @foreach ($ts->steps as $s)
-                                <li style="list-style-type:none">{{ $loop->iteration }}. {!! $s !!}</li>
-                                @endforeach
+                    <div class="col-md-3">
+                        <div class="alert alert-danger d-flex align-items-center" role="alert" style="padding-inline: 30%">
+                            <i  class="bi bi-exclamation-circle-fill me-2 "></i>
+                            <div id="blink2">
+                                {{ $detail->status }}
                             </div>
                         </div>
                     </div>
+                    <div class="detail"> <span class="d-block summery">Silahkan lakukan pembayaran sesuai intruksi di bawah ini</span> </div>
+                    <hr style="width: 100%">
+                    @foreach ($detail->instructions as $ts)
+                        <div class="accordion" id="accordionExample" style="background-color: rgba(204, 203, 202, 0.575)">
+                            <div class="accordion-item mb-3" style="background-color: transparent">
+                                <h2 class="accordion-header" id="headingOne">
+                                    <button class="accordion-button text-dark collapsed" type="button" data-bs-toggle="collapse"
+                                        data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne" style="background-color: transparent; font-weight: bold;">
+                                        {{ $ts->title }}
+                                    </button>
+                                </h2>
+                                <div id="collapseOne" class="accordion-collapse collapse " aria-labelledby="headingOne"
+                                    data-bs-parent="#accordionExample">
+                                    <div class="accordion-body" style="background-color: transparent">
+                                        @foreach ($ts->steps as $s)
+                                        <li style="list-style-type:none" >{{ $loop->iteration }}. {!! $s !!}</li>
+                                        @endforeach
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+        
+                        @endforeach
                 </div>
-
-                @endforeach
+            </div>
+            <div class="col-md-5">
+                <div class="wrap-nota p-4" style="background-color: rgb(221, 220, 219)">
+                    <h4 style="font-family: Marck Script;" class=" theme-color mb-5">Thanks for your order !</h4> <span class="theme-color">Ringkasan Pembayaran</span>
+                    <div class="mb-3">
+                        <hr class="new1">
+                    </div>
+                    <div class="d-flex justify-content-between"> <span class="font-weight-bold">Total order</span> <span class="text-muted">$1750.00</span> </div>
+                    <div class="d-flex justify-content-between"> <small>Pengiriman</small> <small>$175.00</small> </div>
+                    <div class="d-flex justify-content-between mt-3 bg-light p-2"> <span style="font-weight: bold">Total</span> <span class="font-weight-bold theme-color" style="font-weight: bold">Rp. {{ number_format($detail->amount) }}</span> </div>
+                </div>
             </div>
         </div>
+        <script type="text/javascript">
+            var blink = document.getElementById('blink2');
+            setInterval(function () {
+                blink.style.opacity = (blink.style.opacity == 0 ? 1 : 0);
+            }, 800);
+        </script>
 </section>
 @endsection
