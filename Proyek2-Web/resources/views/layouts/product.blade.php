d239b8af0b1669f8df7ccf4270850269a70b4b1b
irfanharfiansyah <irfanharfiansyah37@gmail.com>
2022-03-21 22:46:15 +0700

    Change UI product

--- Proyek2-Web/resources/views/layouts/product.blade.php
+++ Proyek2-Web/resources/views/layouts/product.blade.php
@@ -27,7 +27,7 @@
         <li><a href="/"><i class="bi bi-house-fill"></i> Home</a></li>
         <li><a href="/produk">Katalog Produk</a></li>
     </ol>
-    <div class="container">
+    <div class="container ps-5 pe-5 pt-1" style="background-color: rgba(228, 228, 228, 0.481); border-radius: 40px">
         <div class="list-product">
             <div class="wrap-filter d-flex mt-5 mb-3 align-items-center">
                 <form class="search-bar" action="{{ route('all') }}" method="GET">
@@ -49,7 +49,7 @@
 
                 <input type="hidden" name="cat_id" value="{{ $p->category_id }}">
                 <button type="submit" class="btn btn-murah ">
-                    Harga Termurah <i class="bi bi-sort-down-alt"></i>
+                    Harga Termurah <i class="bi bi-sort-up"></i>
                 </button>
             </form>
             @break
@@ -63,7 +63,7 @@
             <form action="/produk/termurah" method="GET" enctype="multipart/form-data">
                 <input type="hidden" name="cat_id" value="{{ $p->category_id }}">
                 <button type="submit" class="btn btn-murah ">
-                    Harga Termurah <i class="bi bi-sort-down-alt"></i>
+                    Harga Termurah <i class="bi bi-sort-up"></i>
                 </button>
             </form>
             @break
@@ -79,7 +79,7 @@
 
                 <input type="hidden" name="cat_id" value="{{ $p->category_id }}">
                 <button type="submit" class="btn btn-murah ">
-                    Harga Termurah <i class="bi bi-sort-down-alt"></i>
+                    Harga Termurah <i class="bi bi-sort-up"></i>
                 </button>
             </form>
             @break
@@ -94,7 +94,7 @@
             <form action="/produk/termurah" method="GET" enctype="multipart/form-data">
                 <input type="hidden" name="cat_id" value="{{ $p->category_id }}">
                 <button type="submit" class="btn btn-murah ">
-                    Harga Termurah <i class="bi bi-sort-down-alt"></i>
+                    Harga Termurah <i class="bi bi-sort-up"></
                 </button>
             </form>
             @break
@@ -110,37 +110,37 @@
             <form action="/produk/termurah" method="GET" enctype="multipart/form-data">
                 <input type="hidden" name="status" value="murah">
                 <button type="submit" class="btn btn-murah ">
-                    Harga Termurah <i class="bi bi-sort-down-alt"></i>
+                    Harga Termurah <i class="bi bi-sort-up"></i>
                 </button>
         </form>
             @endif
         </div>
 
-        {{-- <form action="/cari" method="GET">
-                <div class="input-group mb-3">
-                    <input type="text" name="search" class="form-control" placeholder="Cari Produk">
-                    <button class="btn btn-outline-secondary" type="submit">Cari</button>
-                </div>
-            </form> --}}
-
-
+        <div class="line"></div>
         <div class="row">
             @forelse ($produk as $p)
-            <div class="col-lg-4 col-md-6 col-12 " style="margin-bottom: 150px">
-                <div class="gallery ">
-                    <a href="/details/{{ $p->slug }}"><img src="{{ asset('/storage/' . $p->featured_image) }}" alt=""
-                            class="img-fluid text-center"></a>
-                </div>
-                <div class="product-det d-flex justify-content-between align-content-center">
-                    <div class="text-pro ">
-                        <p>{{ $p->nama }}</p>
-                        <p><i class="bi bi-bookmarks-fill"></i>{{ $p->category->name }}</p>
+            <div class="col-md-4">
+                <div class="wsk-cp-product" style="font-family: PT Serif">
+                  <div class="wsk-cp-img"><img src="{{ asset('/storage/' . $p->featured_image) }}" alt="Product" class="img-responsive" /></div>
+                  <div class="wsk-cp-text">
+                    <div class="category">
+                      <span>{{ $p->category->name }}</span>
                     </div>
-                    <h4 class="mt-3"> Rp.{{ number_format($p->harga, 0, ',', '.') }}</h4>
+                    <div class="title-product">
+                      <h3>{{ $p->nama }}</h3>
+                    </div>
+                    <div class="description-prod">
+                      <p>{{ $p->keterangan }}</p>
+                    </div>
+                    <div class="product-footer">
+                      <div class="wcf-left"><span class="price"> Rp. {{ number_format($p->harga, 0, ',', '.') }}</span></div>
+                      <div class="wcf-right"><a href="/details/{{ $p->slug }}" class="buy-btn"><i class="bi bi-bag-fill"></i></a></div>
+                    </div>
+                  </div>
                 </div>
-            </div>
+              </div>
             @empty
-            <div class="col-12 text-center mt-5">
+            <div class="col-12 text-center mt-5 mb-5">
                 <h1 style="color: rgb(226, 226, 226)">PRODUK KOSONG</h1>
             </div>
             @endforelse
